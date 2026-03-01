b09d8f4e25ccaaf59040a532a235a01ed1153aa7
juanpagrand <juanepattorres@gmail.com>
2026-03-01 15:16:53 -0500

    fix: correct WebMvcTest import properly

--- src/test/java/com/taller/cicd_demo/CalculatorControllerTest.java
+++ src/test/java/com/taller/cicd_demo/CalculatorControllerTest.java
@@ -2,12 +2,11 @@ package com.taller.cicd_demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(CalculatorController.class)
 class CalculatorControllerTest {
