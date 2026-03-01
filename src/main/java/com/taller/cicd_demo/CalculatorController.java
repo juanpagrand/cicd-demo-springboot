865829165d8b1566f0c9fe750f4a892cf068bdaa
juanpagrand <juanepattorres@gmail.com>
2026-03-01 15:14:16 -0500

    fix: correct WebMvcTest import

--- src/main/java/com/taller/cicd_demo/CalculatorController.java
+++ src/main/java/com/taller/cicd_demo/CalculatorController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+//hola
 @RestController
 @RequestMapping("/api")
 public class CalculatorController {
